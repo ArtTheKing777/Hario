e915087bfeb78336abdb6f4e15802c02e58d0391
ArtTheKing777 <arthur@gruijs.eu>
2023-10-12 11:53:33 +0200

    gloss

---
+++ gloss-counter/gloss-counter/src/Controller.hs
@@ -0,0 +1,35 @@
+
+module Controller where
+
+import Model
+
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import System.Random
+
+-- | Handle one iteration of the game
+step :: Float -> GameState -> IO GameState
+step secs gstate = return $ GameState (ShowAPicture ) 0
+
+getHario :: BitmapData
+getHario = loadBMP(readFile)
+
+step1 :: Float -> GameState -> IO GameState
+step1| elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
+  = -- We show a new random number
+    do randomNumber <- randomIO
+       let newNumber = abs randomNumber `mod` 10
+       return $ GameState (ShowANumber newNumber) 0
+  | otherwise
+  = -- Just update the elapsed time
+    return $ gstate { elapsedTime = elapsedTime gstate + secs }
+
+-- | Handle user input
+input :: Event -> GameState -> IO GameState
+input e gstate = return (inputKey e gstate)
+
+inputKey :: Event -> GameState -> GameState
+inputKey (EventKey (Char c) _ _ _) gstate
+  = -- If the user presses a character key, show that one
+    gstate { infoToShow = ShowAChar c }
+inputKey _ gstate = gstate -- Otherwise keep the same
