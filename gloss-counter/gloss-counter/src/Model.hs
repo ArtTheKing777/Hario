4ef42742084aae5c9feaeeddd13feb5066646431
ArtTheKing777 <arthur@gruijs.eu>
2023-10-24 16:04:56 +0200

    keys set

--- gloss-counter/gloss-counter/src/Model.hs
+++ gloss-counter/gloss-counter/src/Model.hs
@@ -3,15 +3,16 @@
 -- | This module contains the data types
 --   which represent the state of the game
 module Model where
-import Graphics.Gloss (Point)
+import Graphics.Gloss.Interface.IO.Game ( Point, Key )
+import qualified Data.Set as S
 
-data GameState = LevelSelectState { keys::Int,  elapsedTime::Float}
-              | StartScreenState  { keys::Int,  elapsedTime::Float,mousePos::(Float,Float)}
-              | LevelPlayingState { keys::Int,  elapsedTime::Float, 
+data GameState = LevelSelectState { keys::S.Set Key,  elapsedTime::Float}
+              | StartScreenState  { keys::S.Set Key ,  elapsedTime::Float,mousePos::(Float,Float)}
+              | LevelPlayingState { keys::S.Set Key,  elapsedTime::Float, 
                                     hario::Hario}
 
 initialState :: GameState
-initialState = StartScreenState 0 0 (0,0)
+initialState = StartScreenState S.empty 0 (0,0)
 
 data PlayerState = Idle | Walk | Jump | Fall | Die | Victory
 data Looking = Left | Right
