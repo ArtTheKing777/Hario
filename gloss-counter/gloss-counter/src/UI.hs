5e9864ae60c89ba43e11595bdf836504e00f503d
ArtTheKing777 <arthur@gruijs.eu>
2023-10-26 15:38:37 +0200

    bmp

--- gloss-counter/gloss-counter/src/UI.hs
+++ gloss-counter/gloss-counter/src/UI.hs
@@ -1,11 +1,8 @@
 module UI where
-import Controller
 import Fileload
 import qualified Data.Set as S
 
 import Graphics.Gloss.Data.Picture (Picture (Bitmap, BitmapSection, Blank) , pictures, scale, translate, bitmapSection, bitmap)
-import Model
-import Animations
 import Graphics.Gloss.Interface.IO.Animate (animateIO)
 import Data.Fixed
 import GHC.Float (int2Float)
@@ -13,10 +10,16 @@ import Graphics.Gloss
 import qualified Data.Set as S
 import Graphics.Gloss.Interface.IO.Game ( Point, Key (MouseButton), MouseButton (LeftButton) )
 
+data UIElement = Button Picture | SomethingElse Picture
+
+getUIElemtpic::UIElement -> Picture
+getUIElemtpic (Button p) = p
+getUIElemtpic (SomethingElse p) = p
+
 --button buttontext -> scale -> place -> picture
 button:: String -> (Float,Float) -> Point -> IO Picture
 button buttontext (sx,sy) (x,y) = do
-                                    textboxp <- getTextBox
+                                    textboxp <- getTextBoxBmp
                                     let textbox = translate x y (scale sx sy (bitmap textboxp))
                                         textinbox = translate (x-(150*sx)) (y-(57.5*sy)) (scale sx sy (color black(Text buttontext )))
                                     return (pictures [textbox,textinbox])
